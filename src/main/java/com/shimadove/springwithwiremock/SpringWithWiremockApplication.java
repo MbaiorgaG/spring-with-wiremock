ce0013b9737b4d8fb6cfc9913a38bf55288cbbcc
Gabriel Simon Mbaiorga <gabrielmbaiorga@gmail.com>
2022-02-24 18:53:12 +0100

    Initial commit

---
+++ src/main/java/com/shimadove/springwithwiremock/SpringWithWiremockApplication.java
@@ -0,0 +1,13 @@
+package com.shimadove.springwithwiremock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithWiremockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWithWiremockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shimadove/springwithwiremock/SpringWithWiremockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shimadove.springwithwiremock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithWiremockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
