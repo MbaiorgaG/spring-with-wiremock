cdf11828aeb24dcee3e47968b75f4cc74985ff76
Gabriel Simon Mbaiorga <gabrielmbaiorga@gmail.com>
2022-02-24 20:41:29 +0100

    added test

--- src/test/java/com/shimadove/springwithwiremock/SpringWithWiremockApplicationTests.java
+++ src/test/java/com/shimadove/springwithwiremock/SpringWithWiremockApplicationTests.java
@@ -1,9 +1,14 @@
 package com.shimadove.springwithwiremock;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureHttpClient;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest("app.baseUrl= https://localhost:6443")
+@AutoConfigureHttpClient
 class SpringWithWiremockApplicationTests {
 
     @Test
